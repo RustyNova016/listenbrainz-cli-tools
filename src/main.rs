3b6c87ab05e1436f89481b75e4b3ebdce2084ab3
RustyNova <rusty.nova.jsb@gmail.com>
2024-03-01 16:05:47 +0100

    work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use color_eyre::eyre::Ok;
 
+/// This is the module containing all the different tools of this app
+pub mod tools;
+pub mod models;
+
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
---
+++ src/models/mod.rs
@@ -0,0 +1,9 @@
+use listenbrainz::raw::response::UserListensListen;
+
+pub struct UnlinkedListenCollection(Vec<UserListensListen>);
+
+impl UnlinkedListenCollection {
+    pub fn push(&mut self, item: UserListensListen) {
+        if item.
+    }
+}
