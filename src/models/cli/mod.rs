c2baa6344858539d2d208ed145e26beceb08a3b1
rustynova <rusty.nova.jsb@gmail.com>
2024-03-10 02:27:18 +0100

    Refactor clap in own module

---
+++ src/models/cli/mod.rs
@@ -0,0 +1,28 @@
+use clap::{Parser, Subcommand};
+use crate::tools::unlinked::unlinked_command;
+
+/// Tools for Listenbrainz
+#[derive(Parser, Debug, Clone)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum Commands {
+    /// Tools with the unlinked listens
+    Unlinked {
+        /// Name of the user to fetch unlinked listen from
+        #[arg(short, long)]
+        username: String,
+    },
+}
+
+impl Commands {
+    pub fn run(&self) {
+        match self {
+            Commands::Unlinked { username } => unlinked_command(username),
+        }
+    }
+}
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,10 +1,7 @@
-use clap::{Parser, Subcommand};
-
 use listenbrainz::raw::response::UserListensListen;
 
-use crate::tools::unlinked::unlinked_command;
-
 pub mod messy_recording;
+pub mod cli;
 
 pub struct UnlinkedListenCollection(Vec<UserListensListen>);
 
@@ -19,31 +16,5 @@ impl UnlinkedListenCollection {
 impl Extend<UserListensListen> for UnlinkedListenCollection {
     fn extend<T: IntoIterator<Item = UserListensListen>>(&mut self, iter: T) {
         self.0.extend(iter)
-    }
-}
-
-/// Tools for Listenbrainz
-#[derive(Parser, Debug, Clone)]
-#[command(version, about, long_about = None)]
-pub struct Cli {
-    #[command(subcommand)]
-    pub command: Commands,
-}
-
-#[derive(Subcommand, Debug, Clone)]
-pub enum Commands {
-    /// Tools with the unlinked listens
-    Unlinked {
-        /// Name of the user to fetch unlinked listen from
-        #[arg(short, long)]
-        username: String,
-    },
-}
-
-impl Commands {
-    pub fn run(&self) {
-        match self {
-            Commands::Unlinked { username } => unlinked_command(username),
-        }
     }
 }
