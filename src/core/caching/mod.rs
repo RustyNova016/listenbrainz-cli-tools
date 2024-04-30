aa932255b6539e738cca48b6af495ca7ab8f1ab7
RustyNova <rusty.nova.jsb@gmail.com>
2024-04-30 09:55:02 +0200

    Fix creation cache folder

--- src/core/caching/mod.rs
+++ src/core/caching/mod.rs
@@ -2,7 +2,7 @@ pub mod entity_cache;
 pub mod serde_cacache;
 use directories::BaseDirs;
 use once_cell::sync::Lazy;
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 pub static CACHE_LOCATION: Lazy<PathBuf> = Lazy::new(|| {
     let mut path = BaseDirs::new()
@@ -10,5 +10,6 @@ pub static CACHE_LOCATION: Lazy<PathBuf> = Lazy::new(|| {
         .cache_dir()
         .to_path_buf();
     path.push("listenbrainz_cli_tools");
+    fs::create_dir_all(&path).expect("Couldn't create cache directory");
     path
 });
