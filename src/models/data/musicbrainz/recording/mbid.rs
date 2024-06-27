b31a37c05dfdd0df4fb78378d1617dfd476dd2aa
RustyNova <50844553+RustyNova016@users.noreply.github.com>
2024-06-27 15:28:35 +0000

    refactor: added any mbid

--- src/models/data/musicbrainz/recording/mbid.rs
+++ src/models/data/musicbrainz/recording/mbid.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Serialize};
 use crate::core::entity_traits::mb_cached::MBCached;
 use crate::core::entity_traits::mbid::IsMbid;
 use crate::models::data::musicbrainz::external_musicbrainz_entity::ExternalMusicBrainzEntity;
+use crate::models::data::musicbrainz::mbid::any_mbid::AnyMBIDType;
 use crate::models::data::musicbrainz::mbid::generic_mbid::IdAliasState;
 use crate::models::data::musicbrainz::mbid::generic_mbid::MBIDSpe;
 use crate::models::data::musicbrainz::mbid::is_musicbrainz_id::IsMusicbrainzID;
@@ -69,5 +70,14 @@ where
                 .context("Failed to fetch recording from MusicBrainz")?
                 .into_entity(),
         )
+    }
+}
+
+impl<S> From<MBIDSpe<Recording, S>> for AnyMBIDType<S>
+where
+    S: IdAliasState,
+{
+    fn from(value: MBIDSpe<Recording, S>) -> Self {
+        Self::Recording(value)
     }
 }
